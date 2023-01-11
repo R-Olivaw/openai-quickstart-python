43d7579fa64e07290612afc15c81d4e340f1f91b
Daneel Olivaw <34151388+R-Olivaw@users.noreply.github.com>
2023-01-10 22:16:08 -0800

    stuff

--- test.py
+++ test.py
@@ -1,3 +1,39 @@
-import os
+import os, requests, json
 import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
+url = "https://api.openai.com/v1/models/text-curie-001"
+
+#session_id = requests.post(url, headers={"Authorization": f"Bearer {openai.api_key}"}).json()["session"]
+
+prompt = (
+    "My name is Alex!"
+)
+
+params = {
+    "prompt": prompt,
+    "model": "text-curie-001",
+    "max_tokens": 75,
+    "temperature": 0.5,
+}
+
+headers = {"Authorization": f"Bearer {openai.api_key}"}
+response = requests.post(url, json=params, headers=headers)
+
+print(json.dumps(response.json(), indent=2))
+
+prompt = (
+    "What's my name?"
+)
+
+params = {
+    "prompt": prompt,
+    "model": "text-curie-001",
+    "max_tokens": 75,
+    "temperature": 0.5,
+}
+
+headers = {"Authorization": f"Bearer {openai.api_key}"}
+response = requests.post(url, json=params, headers=headers)
+
+print(json.dumps(response.json(), indent=2))
